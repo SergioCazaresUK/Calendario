c470a69518216933ee6d305fc3552b65d933d923
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-04-30 13:13:02 -0700

    Agregar archivos de proyecto.

---
+++ Calendario/Program.cs
@@ -0,0 +1,10 @@
+﻿static void identificar_Mes(int numMes)
+{
+    string[] meses = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+
+    Console.WriteLine($"El mes con numero { numMes } es: {meses[numMes - 1]}");
+};
+
+Console.WriteLine("Ingresa el Numero: ");
+int mesRecibido = int.Parse(Console.ReadLine());
+identificar_Mes(mesRecibido);
